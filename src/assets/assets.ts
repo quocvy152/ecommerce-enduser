59a6ec7c9d843fdbe75d170e93664314627b4a6c
Phạm Quốc Vỹ <phamquocvy@Phams-Mac-mini.local>
2024-05-18 15:42:31 +0700

    init: first commit to config some base in project

---
+++ src/assets/assets.ts
@@ -0,0 +1,18 @@
+import logo from './logo.png';
+
+import product_001 from './product_001.png';
+import product_002 from './product_002.png';
+import product_003 from './product_003.png';
+import product_004 from './product_004.png';
+import product_005 from './product_005.png';
+import product_006 from './product_006.png';
+
+export const images = {
+    logo,
+    product_001,
+    product_002,
+    product_003,
+    product_004,
+    product_005,
+    product_006,
+}
