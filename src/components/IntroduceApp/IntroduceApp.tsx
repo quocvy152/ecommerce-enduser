3d85711bd6f1f01ed27ce950b3bf7468443442c1
Sergio Phạm <SergioPham@Macbook-Pro-cua-Quoc-Vy.local>
2024-06-17 10:20:10 +0700

    feature: make link website have more info

--- src/components/IntroduceApp/IntroduceApp.tsx
+++ src/components/IntroduceApp/IntroduceApp.tsx
@@ -2,21 +2,19 @@ import { Helmet } from 'react-helmet';
 
 const IntroduceApp = () => {
   return (
-    <div>
-      <Helmet>
-        <title>Viva Wool</title>
-        <meta name='description' content='Chào mừng bạn đến với Viva Wool, nơi bạn có thể khám phá và mua những sản phẩm handmade từ len độc đáo, từ áo len đến phụ kiện thời trang. Hãy thưởng thức sự tinh tế và sáng tạo trong từng sản phẩm!' />
-        <meta property='og:title' content='My React App' />
-        <meta property='og:description' content='Chào mừng bạn đến với Viva Wool, nơi bạn có thể khám phá và mua những sản phẩm handmade từ len độc đáo, từ áo len đến phụ kiện thời trang. Hãy thưởng thức sự tinh tế và sáng tạo trong từng sản phẩm!' />
-        <meta property='og:image' content='https://ibb.co/Nyqpsf1' />
-        <meta property='og:url' content='https://vivawool.io.vn' />
-        <meta property='og:type' content='website' />
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta name='twitter:title' content='My React App' />
-        <meta name='twitter:description' content='Chào mừng bạn đến với Viva Wool, nơi bạn có thể khám phá và mua những sản phẩm handmade từ len độc đáo, từ áo len đến phụ kiện thời trang. Hãy thưởng thức sự tinh tế và sáng tạo trong từng sản phẩm!' />
-        <meta name='twitter:image' content='https://ibb.co/Nyqpsf1' />
-      </Helmet>
-    </div>
+    <Helmet>
+      <title>Viva Wool</title>
+      <meta name='description' content='Chào mừng bạn đến với Viva Wool, nơi bạn có thể khám phá và mua những sản phẩm handmade từ len độc đáo, từ áo len đến phụ kiện thời trang. Hãy thưởng thức sự tinh tế và sáng tạo trong từng sản phẩm!' />
+      <meta property='og:title' content='Viva Wool' />
+      <meta property='og:description' content='Chào mừng bạn đến với Viva Wool, nơi bạn có thể khám phá và mua những sản phẩm handmade từ len độc đáo, từ áo len đến phụ kiện thời trang. Hãy thưởng thức sự tinh tế và sáng tạo trong từng sản phẩm!' />
+      <meta property='og:image' content='https://ibb.co/Nyqpsf1' />
+      <meta property='og:url' content='https://vivawool.io.vn' />
+      <meta property='og:type' content='website' />
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:title' content='Viva Wool' />
+      <meta name='twitter:description' content='Chào mừng bạn đến với Viva Wool, nơi bạn có thể khám phá và mua những sản phẩm handmade từ len độc đáo, từ áo len đến phụ kiện thời trang. Hãy thưởng thức sự tinh tế và sáng tạo trong từng sản phẩm!' />
+      <meta name='twitter:image' content='https://ibb.co/Nyqpsf1' />
+    </Helmet>
   )
 }
 
